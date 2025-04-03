3d8f5a04fab9ad1301989e70a763d9f87da8ab4a
Gautam101722 <gautam.krishnakumar2023@vitstudent.ac.in>
2025-04-04 00:16:36 +0530

    helpers

---
+++ MenuList.js
@@ -0,0 +1,40 @@
+
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 350,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 300,
+  },
+  {
+    name: "G & P Special Pizza",
+    image: PedroTechSpecial,
+    price: 450,
+  },
+  {
+    name: "Farm House Pizza",
+    image: Vegan,
+    price: 350,
+  },
+  {
+    name: "Pineapple Punch Pizza",
+    image: Pineapple,
+    price: 400,
+  },
+  {
+    name: "Super sepical G&P Pizza",
+    image: Expensive,
+    price: 600,
+  },
+];
